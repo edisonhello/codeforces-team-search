90e8607e528c961ef3432b729ddee01f55730c9c
edisonhello <edisonhello@hotmail.com>
2019-11-06 18:17:58 +0800

    Initial commit

---
+++ main.js
@@ -0,0 +1,34 @@
+
+
+const fs = require("fs")
+const axios = require("axios")
+const cheerio = require("cheerio")
+
+argv = process.argv
+
+team_info = {}
+
+let ST = parseInt(argv[2]), ED = parseInt(argv[3])
+
+function query(i) {
+	if (i == ED + 1) {
+		let data = JSON.stringify(team_info, null, 4)
+		data = "<pre>" + data + "</pre>"
+		fs.writeFileSync(`${argv[4]}.html`, data)
+	} else {
+		axios.get(`http://codeforces.com/team/${i}`).then((res) => {
+			$ = cheerio.load(res.data)
+			let teamname = $('h3').text()
+			let member = []
+			$(".members tbody ul").children().each(function(i, e) {
+				let name = $(e).children("div").text().split('\n')[3].split(' ')[8]
+				member.push(name)
+			})
+			team_info[i] = [teamname, member]
+			console.log(i, team_info[i])
+			query(i + 1)
+		})
+	}
+}
+
+query(ST)
---
+++ run.sh
@@ -0,0 +1,6 @@
+ node main.js 1 10000 team0 &
+ node main.js 10001 20000 team1 &
+ node main.js 20001 30000 team2 &
+ node main.js 30001 40000 team3 &
+ node main.js 40001 50000 team4 &
+ node main.js 50001 60000 team5 &
